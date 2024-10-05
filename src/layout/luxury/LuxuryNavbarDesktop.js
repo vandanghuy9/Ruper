2ce3fd913b3366dec95b00e1530df5dcb7e76c2b
vandanghuy9 <vandanghuy9@gmail.com>
2024-10-05 12:54:25 +0700

    luxury page

---
+++ src/layout/luxury/LuxuryNavbarDesktop.js
@@ -0,0 +1,34 @@
+"use client";
+import React, { useEffect } from "react";
+import { Logo, NavbarDesktopMenu, HeaderPageLink } from "@component/layout";
+import { useShopProduct } from "@context/ShopProductContext";
+const LuxuryNavbarDesktop = ({ blogList, sampleProduct }) => {
+  const { setBlogList } = useShopProduct();
+  useEffect(() => {
+    setBlogList(blogList);
+  }, [blogList]);
+
+  return (
+    <div className="header-v3 relative">
+      <div className="header-desktop">
+        <div className="header-wrapper">
+          <div className="section-padding">
+            <div className="section-container p-l-r">
+              <div className="row">
+                <div className="col-xl-6 col-lg-8 col-md-12 col-sm-12 col-12 header-left">
+                  <Logo />
+                  <NavbarDesktopMenu blogList={blogList} sampleProduct={sampleProduct} />
+                </div>
+                <div className="col-xl-6 col-lg-4 col-md-12 col-sm-12 col-12 header-right">
+                  <HeaderPageLink />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LuxuryNavbarDesktop;
